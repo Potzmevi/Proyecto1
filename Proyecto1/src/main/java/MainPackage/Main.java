506d0cf177911ea6172d5e3d260a9959cedf7604
= <=>
2020-08-04 19:36:54 -0600

    Login e ingresar

---
+++ Proyecto1/src/main/java/MainPackage/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MainPackage;
+
+import FrontEnd.Ingresar;
+import FrontEnd.Login;
+
+
+/**
+ *
+ * @author potz
+ */
+public class Main {
+    public static void main(String[] args) {
+        Ingresar ingresar = new Ingresar();
+        ingresar.setVisible(true);
+    }
+    
+    
+}
